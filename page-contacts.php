5be7e49aa57d4a7ab6cc003b047bf4177476aac5
martinmlad1985 <60547325+martinmlad1985@users.noreply.github.com>
2022-08-08 22:24:29 +0300

    sm change

--- page-contacts.php
+++ page-contacts.php
@@ -2,8 +2,10 @@
 <?php get_header(); ?>
 
 <main>
-    <div class="container py-5">    
-    <?php echo do_shortcode( '[contact-form-7 id="196" title="Форма2"]' );?> 
+    <div class="container py-5">
+        <div class="col col-sm-12 col-md-10 col-lg-8 col-xl-6 mx-auto">  
+             <?php echo do_shortcode( '[contact-form-7 id="196" title="Форма2"]' );?> 
+        </div>
     </div>
 </main>
 
